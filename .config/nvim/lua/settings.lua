03e5cc3e68e4fd407fe6982be63ca8896942f5f6
Vinícius Gonçalves de Oliveira <viniciusp.olivera@gmail.com>
2021-05-17 16:32:41 -0300

    Fixed buffer and window options not apllying

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,14 +1,33 @@
 local GLOBAL = vim.o
 local BUFFER = vim.bo
 local WINDOW = vim.wo
+local options = {}
 
-local set_options = function(locality, options)
-	for key, value in pairs(options) do
+local set_options = function(locality, opt)
+	for key, value in pairs(opt) do
 		locality[key] = value
 	end
 end
 
-local global_options = {
+local set_globals = function(opt)
+	for _, value in pairs(opt) do
+		for key, val in pairs(value) do
+			GLOBAL[key] = val
+		end
+	end
+end
+
+options['buffer'] = {
+	shiftwidth = 4,
+	tabstop = 4,
+}
+
+options['window'] = {
+	number = true,
+	cursorline = true,
+}
+
+options['global'] = {
 	showmode = false,
 	hidden = true,
 	hlsearch = false,
@@ -16,19 +35,9 @@ local global_options = {
 	splitright = true,
 }
 
-local buffer_options = {
-	shiftwidth = 4,
-	tabstop = 4,
-}
-
-local window_options = {
-	number = true,
-	cursorline = true,
-}
-
-set_options(GLOBAL, global_options)
-set_options(BUFFER, buffer_options)
-set_options(WINDOW, window_options)
+set_globals(options)
+set_options(BUFFER, options['buffer'])
+set_options(WINDOW, options['window'])
 
 -- Norme.nvim options
 vim.cmd "autocmd BufEnter,BufRead *.c,*.h lua require('norme').lint()"
