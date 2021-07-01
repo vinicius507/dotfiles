e9a4de4a130ab87a683c94536d1fc7b3b2b083ba
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-07-01 19:59:42 -0200

    fix: spotifyd notification on startup

--- .config/awesome/notifications/spotify.lua
+++ .config/awesome/notifications/spotify.lua
@@ -1,16 +1,21 @@
 local notifications = require('notifications')
 
 local notif
+local first_time = true
 
 awesome.connect_signal('spotify::song',
 	function (song, artist)
 		-- Send notification
-		local message = song .. ' - ' .. artist
-		notif = notifications.notify({
-			title = 'Now Playing',
-			message = message,
-			app_name = 'spotify',
-			urgency = 'low'
-		}, notif)
+		if first_time then
+			first_time = false
+		else
+			local message = song .. ' - ' .. artist
+			notif = notifications.notify({
+				title = 'Now Playing',
+				message = message,
+				app_name = 'spotify',
+				urgency = 'low'
+			}, notif)
+		end
 	end
 )
