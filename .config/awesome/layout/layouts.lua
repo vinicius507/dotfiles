150f2002cf8d22f28f0a2f4658a4eea21d4e14b2
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-07-07 17:29:30 -0300

    add: tile right layout

--- .config/awesome/layout/layouts.lua
+++ .config/awesome/layout/layouts.lua
@@ -3,8 +3,9 @@ local awful = require('awful')
 local set_layouts = function ()
     awful.layout.append_default_layouts({
         awful.layout.suit.spiral.dwindle,
-        awful.layout.suit.floating,
+        awful.layout.suit.tile.right,
         awful.layout.suit.max,
+        awful.layout.suit.floating,
     })
 end
 
